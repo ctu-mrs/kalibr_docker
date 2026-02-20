ce7a194eda66332caba31cb310693ae20f6cec7e
Filip <filipmymail@gmail.com>
2026-02-20 02:21:27 +0100

    Improve quoting

--- input_conversion/run.sh
+++ input_conversion/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-bag_file="$(realpath ${1:-'ros.mcap'})"
+bag_file="$(realpath "${1:-ros.mcap}")"
 bag_file_name="$(basename "$bag_file")"
-image_topic="${2:-'/camera/rgb/image_raw'}"
+image_topic="${2:-/camera/rgb/image_raw}"
 
 this_dir="$(dirname "$(realpath "$0")")"
-output_dir="${3:-"$this_dir/../compose_session/data/camera"}"
+output_dir="${3:-$this_dir/../compose_session/data/camera}"
 
 ros='jazzy'
 if [ "$(head -c 4 "$bag_file" 2> /dev/null)" = '#ROS' ]; then
@@ -17,4 +17,4 @@ docker run --rm \
   -v "$this_dir/scripts/extract_images_ros_$ros.py":/tmp/extract_images_ros.py \
   -v "$output_dir":/tmp/camera \
   ctumrs/ros_$ros:latest python3 \
-  /tmp/extract_images_ros.py "/tmp/$bag_file_name" $image_topic /tmp/camera
+  /tmp/extract_images_ros.py "/tmp/$bag_file_name" "$image_topic" /tmp/camera
